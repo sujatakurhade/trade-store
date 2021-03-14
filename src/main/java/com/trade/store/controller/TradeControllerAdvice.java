a886833b4502d7150e3559d690966c6ca043058e
Sujata Kurhade <sujatackurhade@gmail.com>
2021-03-15 01:38:34 +0530

    Fixed Controller Advice for correct status code

--- src/main/java/com/trade/store/controller/TradeControllerAdvice.java
+++ src/main/java/com/trade/store/controller/TradeControllerAdvice.java
@@ -3,6 +3,9 @@ package com.trade.store.controller;
 import com.trade.store.exception.EntityViolationException;
 import com.trade.store.exception.InvalidTradeException;
 import com.trade.store.model.ExceptionTradeResponse;
+import com.trade.store.service.impl.TradeServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,19 +14,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class TradeControllerAdvice {
+    private Logger LOG = LoggerFactory.getLogger(TradeControllerAdvice.class);
+
     @ExceptionHandler(InvalidTradeException.class)
     @ResponseBody
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ExceptionTradeResponse handleInvalidTradeException(InvalidTradeException se) {
-        ExceptionTradeResponse response = new ExceptionTradeResponse(se.getMessage());
-        return response;
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    public ExceptionTradeResponse handleInvalidTradeException(InvalidTradeException ex) {
+        LOG.error("Invalid Trade exception", ex);
+        return new ExceptionTradeResponse(ex.getMessage());
     }
 
     @ExceptionHandler(EntityViolationException.class)
     @ResponseBody
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ExceptionTradeResponse handleEntityViolationException(InvalidTradeException se) {
-        ExceptionTradeResponse response = new ExceptionTradeResponse(se.getMessage());
-        return response;
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ExceptionTradeResponse handleEntityViolationException(InvalidTradeException ex) {
+        LOG.error("Entity violation exception", ex);
+        return new ExceptionTradeResponse(ex.getMessage());
     }
 }
